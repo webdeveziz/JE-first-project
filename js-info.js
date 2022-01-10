20d7cdf3d05746d2883e4726671a99039e141bbb
webdeveziz <38165397+webdeveziz@users.noreply.github.com>
2022-01-10 23:23:45 +0300

    Add files via upload

---
+++ js-info.js
@@ -0,0 +1,16 @@
+//Кто разработал язык программирования JavaScript?
+alert(
+  'JavaScript разработал Brendan Eich, сооснователь проекта Mozilla Corporation, работавшим на Netscape в далёком 1995 году.'
+)
+
+//За сколько дней был разработан JavaScript?
+alert('JavaScript был создан за 10 дней')
+
+//В каком году появился JavaScript?
+alert('JavaScript появился в сентябре 1995 года')
+
+//JavaScript - это то же самое, что и Java? (отвечайте либо “JavaScript это Java”, либо “JavaScript это не Java”)
+alert('JavaScript это не Java')
+
+//Как называется спецификация JavaScript?
+alert('Спецификация языка JavaScript называется ECMAScript')
